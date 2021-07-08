3c74da1970b83e93048e82cda033ec09b7e192b2
ana marina <marinabrandao@maxtrack.com.br>
2021-07-08 10:21:14 -0300

    questão 2 e 3

--- trabalho/trabalho_sistemas_controlados.m
+++ trabalho/trabalho_sistemas_controlados.m
@@ -9,7 +9,6 @@ close all
 
 
 s = tf('s');
-
 num = 2;
 den = [50 15 1];
 g = tf(num,den,'InputDelay',1)
@@ -17,6 +16,16 @@ g = tf(num,den,'InputDelay',1)
 figure(1)
 step(g);
 
-figure(2)
-%rlocus(g);
+[z,p,k] = tf2zp(num,den);
+%fvtool(num,den,'polezero')
+zplane(z,p);
+text(real(z)+.1,imag(z),'Zero')
+text(real(p)+.1,imag(p),'Polo')
+
+gd = c2d(g, ,'zoh'); %o segundo parâmetro em vazio
+                     %é o "sample time" e
+                     %deve ser determinado para ter
+                     %10 amostras por constante de 
+                     %tempo
+
 
