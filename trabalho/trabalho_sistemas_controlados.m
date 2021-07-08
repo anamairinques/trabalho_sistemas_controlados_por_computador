d6e4ef9657b750f773c6aada42bc412c854a2b2c
ana marina <marinabrandao@maxtrack.com.br>
2021-07-08 10:36:52 -0300

    imagem 1 e imagem 2

--- trabalho/trabalho_sistemas_controlados.m
+++ trabalho/trabalho_sistemas_controlados.m
@@ -18,11 +18,12 @@ step(g);
 
 [z,p,k] = tf2zp(num,den);
 %fvtool(num,den,'polezero')
+figure(2);  
 zplane(z,p);
 text(real(z)+.1,imag(z),'Zero')
 text(real(p)+.1,imag(p),'Polo')
 
-gd = c2d(g, ,'zoh'); %o segundo parâmetro em vazio
+gd = c2d(g,0.1,'zoh'); %o segundo parâmetro em vazio
                      %é o "sample time" e
                      %deve ser determinado para ter
                      %10 amostras por constante de 
